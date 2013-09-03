bf345c1401e55810e3c2986a32d8da29d7e258d0
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-09-03 13:55:41 +0300

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ratetea::Application.config.secret_key_base = '914fad4c7d496d9a061a3ce9a24183f597765c4f6666e6daedddb7df3ca197a02be75ef65fc8c9328588043c3e056a41359183fc0fbb5c1cd0f7293ede718a22'
