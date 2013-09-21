7af0f13836f2c627aa8abf57479cd60ee577b3ac
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-09-21 12:41:58 +0300

    Code style

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,8 +1,9 @@
 class Rating < ActiveRecord::Base
 
-  validates_numericality_of :score, { :greater_than_or_equal_to => 1,
-                                      :less_than_or_equal_to => 50,
-                                      :only_integer => true }
+  validates_numericality_of :score,
+                            :only_integer => true,
+                            :greater_than_or_equal_to => 1,
+                            :less_than_or_equal_to => 50
 
   belongs_to :tea
   belongs_to :user
